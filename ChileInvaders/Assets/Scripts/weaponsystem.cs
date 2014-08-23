579bd9c7694e5129a8e106b897b39e24424065c2
Federico Ramundo <framundo@Federicos-MacBook-Pro.local>
2014-08-23 17:39:45 -0300

    rm conditional platform if

--- ChileInvaders/Assets/Scripts/weaponsystem.cs
+++ ChileInvaders/Assets/Scripts/weaponsystem.cs
@@ -2,8 +2,7 @@
 using System.Collections;
 
 public class weaponsystem : MonoBehaviour {
-	
-	
+
 	//here are public variables that are accessed in the inspector like the bullet we shoot, the sound when we shoot it, and how fast it shoots.
 	public GameObject bullet1;
 	public AudioClip shotSound;
@@ -17,35 +16,6 @@ public class weaponsystem : MonoBehaviour {
 		//here we keep track of time that is applied to the counter so we can limit the fire rate.
 		counter += Time.deltaTime;
 		
-		#if UNITY_WEBPLAYER
-		//keyboard controls for web versions that will allow the bullet to shoot
-		if(Input.GetKey("space")){
-			if(counter > 1/fireRate){
-				//we decide after our script allows a bullet to be fired, what variation should be shot, more can be added for any level
-				if(level == 1){
-					Instantiate(bullet1, transform.position+Vector3(0.5f,-0.1f,0), Quaternion.Euler(-90,0,0));
-				}
-				if(level == 2){
-					Instantiate(bullet1, transform.position+Vector3(0.5f,-0.1f,0.5f), Quaternion.Euler(-90,0,0));
-					Instantiate(bullet1, transform.position+Vector3(0.5f,-0.1f,-0.5f), Quaternion.Euler(-90,0,0));
-				}
-				if(level >= 3){
-					Instantiate(bullet1, transform.position+Vector3(0.5f,-0.1f,0.25f), Quaternion.Euler(-90,0,0));
-					Instantiate(bullet1, transform.position+Vector3(0.5f,-0.1f,-0.25f), Quaternion.Euler(-90,0,0));
-					FIXME_VAR_TYPE custom1= Instantiate(bullet1, transform.position+Vector3(0.5f,-0.1f,0.5f), Quaternion.Euler(-90,0,0));
-					FIXME_VAR_TYPE custom2= Instantiate(bullet1, transform.position+Vector3(0.5f,-0.1f,-0.5f), Quaternion.Euler(-90,0,0));
-					custom1.transform.rigidbody.velocity.z = 3;
-					custom2.transform.rigidbody.velocity.z = -3;
-				}
-				//play the bullet sound
-				audio.PlayOneShot(shotSound);
-				//we want to reset the counter so that our fire rate always applies
-				counter = 0.0f;
-			}
-		}
-		#endif
-		
-		#if UNITY_STANDALONE
 		//keyboard controls for desktop versions that will allow the bullet to shoot
 		if(Input.GetKey("space")){
 			if(counter > 1/fireRate){
@@ -71,64 +41,6 @@ public class weaponsystem : MonoBehaviour {
 				counter = 0.0f;
 			}
 		}
-		#endif
-		
-		#if UNITY_ANDROID
-		//mobile controls for android that will allow bullet to shoot
-		if(Input.touchCount > 0){
-			if(counter > 1/fireRate){
-				//we decide after our script allows a bullet to be fired, what variation should be shot, more can be added for any level
-				if(level == 1){
-					Instantiate(bullet1, transform.position+Vector3(0.5f,-0.1f,0), Quaternion.Euler(-90,0,0));
-				}
-				if(level == 2){
-					Instantiate(bullet1, transform.position+Vector3(0.5f,-0.1f,0.5f), Quaternion.Euler(-90,0,0));
-					Instantiate(bullet1, transform.position+Vector3(0.5f,-0.1f,-0.5f), Quaternion.Euler(-90,0,0));
-				}
-				if(level >= 3){
-					Instantiate(bullet1, transform.position+Vector3(0.5f,-0.1f,0.25f), Quaternion.Euler(-90,0,0));
-					Instantiate(bullet1, transform.position+Vector3(0.5f,-0.1f,-0.25f), Quaternion.Euler(-90,0,0));
-					FIXME_VAR_TYPE custom1= Instantiate(bullet1, transform.position+Vector3(0.5f,-0.1f,0.5f), Quaternion.Euler(-90,0,0));
-					FIXME_VAR_TYPE custom2= Instantiate(bullet1, transform.position+Vector3(0.5f,-0.1f,-0.5f), Quaternion.Euler(-90,0,0));
-					custom1.transform.rigidbody.velocity.z = 3;
-					custom2.transform.rigidbody.velocity.z = -3;
-				}
-				//play the bullet sound
-				audio.PlayOneShot(shotSound);
-				//we want to reset the counter so that our fire rate always applies
-				counter = 0.0f;
-			}
-		}
-		#endif
-		
-		#if UNITY_IPHONE
-		//mobile controls for ios that will allow bullet to shoot
-		if(Input.touchCount > 0){
-			if(counter > 1/fireRate){
-				//we decide after our script allows a bullet to be fired, what variation should be shot, more can be added for any level
-				if(level == 1){
-					Instantiate(bullet1, transform.position+Vector3(0.5f,-0.1f,0), Quaternion.Euler(-90,0,0));
-				}
-				if(level == 2){
-					Instantiate(bullet1, transform.position+Vector3(0.5f,-0.1f,0.5f), Quaternion.Euler(-90,0,0));
-					Instantiate(bullet1, transform.position+Vector3(0.5f,-0.1f,-0.5f), Quaternion.Euler(-90,0,0));
-				}
-				if(level >= 3){
-					Instantiate(bullet1, transform.position+Vector3(0.5f,-0.1f,0.25f), Quaternion.Euler(-90,0,0));
-					Instantiate(bullet1, transform.position+Vector3(0.5f,-0.1f,-0.25f), Quaternion.Euler(-90,0,0));
-					FIXME_VAR_TYPE custom1= Instantiate(bullet1, transform.position+Vector3(0.5f,-0.1f,0.5f), Quaternion.Euler(-90,0,0));
-					FIXME_VAR_TYPE custom2= Instantiate(bullet1, transform.position+Vector3(0.5f,-0.1f,-0.5f), Quaternion.Euler(-90,0,0));
-					custom1.transform.rigidbody.velocity.z = 3;
-					custom2.transform.rigidbody.velocity.z = -3;
-				}
-				//play the bullet sound
-				audio.PlayOneShot(shotSound);
-				//we want to reset the counter so that our fire rate always applies
-				counter = 0.0f;
-			}
-		}
-		#endif
-		
 		//end of function update
 	}
 	
