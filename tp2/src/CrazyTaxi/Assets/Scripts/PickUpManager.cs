884b67768c642ce60adc6b13a41b41edc031e42e
ConcoMB <cmaderblanco@gmail.com>
2014-09-06 16:40:05 -0300

    starting

---
+++ tp2/src/CrazyTaxi/Assets/Scripts/PickUpManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpManager : MonoBehaviour {
+
+	private bool isInPickupMode;
+
+	void pickUpModeOn() {
+		isInPickupMode = true;		
+		foreach (GameObject bullet in GameObject.FindGameObjectsWithTag("PickUpZone")) {
+				}
+
+	}
+
+	void pickUpModeOff() {
+		isInPickupMode = false;		
+		Debug.Log ("off");
+	}
+}
---
+++ tp2/src/CrazyTaxi/Assets/Scripts/PickUpZone.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpZone : MonoBehaviour {
+	
+	private bool isInBox;
+
+	void OnTriggerEnter (Collider other) {
+		isInBox = true;
+	}
+
+	void OnTriggerExit (Collider other) {
+		isInBox = false;
+	}
+	
+	void Update() {
+		if (Input.GetKey("space")) {
+			GameObject car = GameObject.FindGameObjectWithTag("Player");
+			if (car.rigidbody.velocity.magnitude < 0.05) {
+				GameObject.Find("PickUpManager").SendMessage("pickUpModeOn", SendMessageOptions.DontRequireReceiver);
+			}
+		}
+	}
+}
