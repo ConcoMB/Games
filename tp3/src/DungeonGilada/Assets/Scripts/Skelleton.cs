f2c69619268986ee58c1dc1bfad5da744e59f6ae
ConcoMB <cmaderblanco@gmail.com>
2014-10-05 19:09:55 -0300

    si

---
+++ tp3/src/DungeonGilada/Assets/Scripts/Skelleton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Skelleton : MonoBehaviour {
+
+	public int moveSpeed = 3;
+	public int rotationSpeed = 3;
+	private Transform target;
+
+	void Start () {
+		target = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+	
+	void Update () {
+		transform.rotation = Quaternion.Slerp (transform.rotation, 
+		                                      Quaternion.LookRotation (target.position - transform.position), 
+		                                      rotationSpeed * Time.deltaTime);
+//		transform.position = transform.forward * moveSpeed * Time.deltaTime;
+	}
+}
