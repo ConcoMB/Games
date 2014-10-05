efe77b79162b30689bd8e75e0c03bf418218cee6
Federico Ramundo <framundo@Federicos-MacBook-Pro.local>
2014-10-05 18:15:23 -0300

    level generator

---
+++ tp3/src/DungeonGilada/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelGenerator : MonoBehaviour {
+
+	public GameObject spawnRoom;
+	public Transform[] rooms;
+
+	// Use this for initialization
+	void Start () {
+		GameObject mountPoint = spawnRoom.transform.FindChild("Mount Point").gameObject;
+		for (int i = 0; i < 5; i++) {
+			int index = Random.Range(0, rooms.Length - 1);
+			Debug.Log (index);
+			Transform room = (Transform) Instantiate(rooms[index], mountPoint.transform.position, mountPoint.transform.rotation);
+			mountPoint = room.FindChild("Mount Point").gameObject;
+		}
+	}
+}
