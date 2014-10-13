70cecb420e1c2bffa90a5531564e4e8437bcf608
ConcoMB <cmaderblanco@gmail.com>
2014-10-13 17:24:53 -0300

    ee

--- tp3/src/DungeonGilada/Assets/Scripts/LevelGenerator.cs
+++ tp3/src/DungeonGilada/Assets/Scripts/LevelGenerator.cs
@@ -11,7 +11,6 @@ public class LevelGenerator : MonoBehaviour {
 		GameObject mountPoint = spawnRoom.transform.FindChild("Mount Point").gameObject;
 		for (int i = 0; i < 5; i++) {
 			int index = Random.Range(0, rooms.Length - 1);
-			Debug.Log (index);
 			Transform room = (Transform) Instantiate(rooms[index], mountPoint.transform.position, mountPoint.transform.rotation);
 			mountPoint = room.FindChild("Mount Point").gameObject;
 		}
--- tp3/src/DungeonGilada/Assets/Scripts/Skelleton.cs
+++ tp3/src/DungeonGilada/Assets/Scripts/Skelleton.cs
@@ -5,25 +5,55 @@ public class Skelleton : MonoBehaviour {
 
 	public int moveSpeed = 3;
 	public int rotationSpeed = 3;
+	public int health = 2;
 	private Transform target;
+	private bool deadStart;
+	private Status status;
+
+	public enum Status{Idle, Attacking, Hit, Dead}
 
 	void Start () {
 		target = GameObject.FindGameObjectWithTag ("Player").transform;
+		AnimationEvent e = new AnimationEvent();
+		e.functionName = "SetNoMoreHit";
+		e.time = animation.GetClip("gethit").length;
+		animation.GetClip("gethit").AddEvent(e);
 	}
 	
 	void Update () {
+		if (status == Status.Dead || status == Status.Hit) {
+			return;
+		}
 		float distance = Vector3.Distance (transform.position, target.position);
 		transform.rotation = Quaternion.Slerp (transform.rotation, 
-                      Quaternion.LookRotation (target.position - transform.position), 
-                      rotationSpeed * Time.deltaTime);
-		if (distance < 2) {
+		      Quaternion.LookRotation (target.position - transform.position), 
+		      rotationSpeed * Time.deltaTime);
+		transform.position = new Vector3 (transform.position.x, 0, transform.position.z);
+		if (deadStart) {
+			animation.Play ("die");
+			status = Status.Dead;
+		} else if (distance < 3) {
 			animation.Play ("attack");
-
 		} else if (distance < 10) {
 			animation.Play ("run");
 			transform.position += transform.forward * moveSpeed * Time.deltaTime;
 		} else {
 			animation.Play("idle");
 		}
+	}
+
+	void Hit(int strenght) {
+		Debug.Log (health);
+		health -= strenght;
+		if (health < 0) {
+			deadStart = true;
+		} else {
+			animation.Play ("gethit");
+			status = Status.Hit;
+		}
+	}
+
+	void SetNoMoreHit() {
+		status = Status.Idle;
 	}
 }
