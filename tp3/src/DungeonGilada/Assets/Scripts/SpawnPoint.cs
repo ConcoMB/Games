bd1e52388a7a5ab73e242e07aee12e28b3842cd6
Federico Ramundo <framundo@Federicos-MacBook-Pro.local>
2014-10-19 19:01:41 -0300

    SpawnPoints in rooms & skelletton scripts & final room fix

---
+++ tp3/src/DungeonGilada/Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPoint : MonoBehaviour {
+	public GameObject[] spawnObjects; 
+	public float probability;
+
+	// Use this for initialization
+	void Start () {
+		if (Random.value < probability) {
+			int index = Random.Range(0, spawnObjects.Length);
+			Debug.Log (index);
+			GameObject spawned = (GameObject) Instantiate (spawnObjects[index], transform.position, transform.rotation);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
