8ee2007eca6dea5a3af66d0c5f80de0f06de9ac0
Chino <joseignaciosg9@gmail.com>
2014-11-16 18:26:00 -0300

    assetsCharacters

---
+++ tp4/src/weParty/Assets/TinyMedievalPack/Scripts/MenuControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuControl : MonoBehaviour {
+    UISprite menuOn, menuOff;
+    public UILabel label;
+
+	void Awake () {
+        menuOff = transform.Find("BackgroundOff").GetComponent<UISprite>();
+        menuOn = transform.Find("BackgroundOn").GetComponent<UISprite>();
+        label = GetComponentInChildren<UILabel>();
+    }
+
+    public void OnMenu()
+    {
+        menuOff.enabled = false;
+        menuOn.enabled = true;
+        label.color = Color.white;
+    }
+
+    public void OffMenu()
+    {
+        menuOff.enabled = true;
+        menuOn.enabled = false;
+        label.color = new Color(0.6f, 0.6f, 0.6f);
+    }
+
+}
